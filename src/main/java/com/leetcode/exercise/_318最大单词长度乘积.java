4367876d6c2a9efda4fcc48375c2f4dd60cc169a
wangyj <you@example.com>
2021-11-17 16:23:34 +0800

    dd

---
+++ src/main/java/com/leetcode/exercise/_318最大单词长度乘积.java
@@ -0,0 +1,14 @@
+package com.leetcode.exercise;
+
+/**
+ * @author wangyj
+ * @Description: (最大单词长度乘积)
+ * https://leetcode-cn.com/problems/maximum-product-of-word-lengths/
+ * @date 2021/11/17 4:23 下午
+ */
+public class _318最大单词长度乘积 {
+
+    public int maxProduct(String[] words) {
+
+    }
+}
